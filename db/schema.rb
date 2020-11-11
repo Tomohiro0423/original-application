bb220e2d848e512e714349f5783c43201592a2a3
tomohiro.yamada <tomohiro1996423@gmail.com>
2020-11-11 14:49:50 +0000

    post_model_migrate_rev2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_141656) do
+ActiveRecord::Schema.define(version: 2020_11_11_142711) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
