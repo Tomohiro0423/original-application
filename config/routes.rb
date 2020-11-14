e4f02443a1f478e0941b7164128b2508bf49b27e
tomohiro.yamada <tomohiro1996423@gmail.com>
2020-11-14 06:59:06 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   
   resources :posts
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 end
